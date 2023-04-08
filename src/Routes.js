7ca81bfdd88173ae76e3dd23d51df0684a76a028
Saifali786 <saifaliprasla786@gmail.com>
2023-04-08 19:10:23 -0300

    Adding frontend code

---
+++ src/Routes.js
@@ -0,0 +1,15 @@
+
+// import React from 'react';
+// import { Route, Routes as Rt } from 'react-router-dom';
+// import RequestPickup from './components/RequestPickup';
+// import ContactUs from './components/ContactUs';
+// const Routes = () => {
+//     return (
+//       <Rt>
+//         <Route path="/requestpickup" element={<RequestPickup />} />
+//         <Route path="/contactus" element={<ContactUs />}/>  
+  
+//         </Rt>
+//   );
+//   }
+//   export default Routes;
